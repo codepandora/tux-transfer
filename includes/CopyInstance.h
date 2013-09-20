b6103bf0507c1b4ca8b24ba58937a0389e5e681a
DroidBoyJr <sam.linuxfriend@gmail.com>
2013-09-20 21:35:58 +0530

    single source directory copy without multithreading working

---
+++ includes/CopyInstance.h
@@ -0,0 +1,43 @@
+
+#ifndef CopyInstance
+#define CopyInstance
+
+#include<iostream>
+#include<sys/stat.h>
+#include<string.h>
+#include<malloc.h>
+#include<dirent.h>
+#include<stdlib.h>
+#include<fstream>
+#include<boost/thread.hpp>
+#include<boost/thread/thread.hpp>
+#define NEWDIR ( dir* ) malloc( sizeof( dir ) )
+#define NEWFILE ( file* ) malloc( sizeof( file ) )
+
+using namespace std;
+
+
+class copyInstance
+{		
+
+		char* source, destination, pathToSource, command;
+		char buffer[512];
+		int sourceType;
+		long totalBytesToCopy;
+		void initializeSourceDirStructure();
+		bool isDir( const char* );
+		bool isFile( const char*, long* );
+		void copyWork( string, string );
+		void testPrint();
+		void reader();
+		void writer();
+		ifstream readerStream;
+		ofstream writerStream;
+
+	public:
+		copyInstance( const char*,  const char* );
+
+};
+
+#endif
+
